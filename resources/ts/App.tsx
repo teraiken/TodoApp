cef024a8c1816c4ec9529b939cf765e1f0c49052
teraiken <terai.ken@relation2012.com>
2024-02-28 08:51:48 +0900

    AxiosとReactQueryでAPIからデータを取得する

--- resources/ts/App.tsx
+++ resources/ts/App.tsx
@@ -1,8 +1,24 @@
 import { FC } from "react";
 import Router from "./router";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 const App: FC = () => {
-    return <Router />;
+    const queryClient = new QueryClient({
+        defaultOptions: {
+            queries: {
+                retry: false,
+            },
+            mutations: {
+                retry: false,
+            },
+        },
+    });
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            <Router />
+        </QueryClientProvider>
+    );
 };
 
 export default App;
--- resources/ts/pages/tasks/index.tsx
+++ resources/ts/pages/tasks/index.tsx
@@ -1,6 +1,37 @@
 import { FC } from "react";
+import axios from "axios";
+import { Task } from "../../types/api/Task";
+import { useQuery } from "react-query";
 
 const TaskPage: FC = () => {
+    /*
+    const [tasks, setTasks] = useState<Task[]>([]);
+
+    const getTasks = async () => {
+        const { data } = await axios.get<Task[]>("api/tasks");
+        setTasks(data);
+    };
+
+    useEffect(() => {
+        getTasks();
+    }, []);
+    */
+
+    const { data: tasks, status } = useQuery("tasks", async () => {
+        const { data } = await axios.get<Task[]>("api/tasks");
+        return data;
+    });
+
+    if (status === "loading") {
+        return <div className="loader" />;
+    } else if (status === "error") {
+        return (
+            <div className="align-center">データの読み込みに失敗しました。</div>
+        );
+    } else if (!tasks || tasks.length <= 0) {
+        return <div className="align-center">登録されたTODOはありません。</div>;
+    }
+
     return (
         <>
             <form className="input-form">
@@ -9,7 +40,7 @@ const TaskPage: FC = () => {
                         type="text"
                         className="input"
                         placeholder="TODOを入力してください。"
-                        value=""
+                        defaultValue=""
                     />
 
                     <button className="btn is-primary">追加</button>
@@ -18,55 +49,20 @@ const TaskPage: FC = () => {
 
             <div className="inner">
                 <ul className="task-list">
-                    <li>
-                        <label className="checkbox-label">
-                            <input type="checkbox" className="checkbox-input" />
-                        </label>
-                        <div>
-                            <span>新しいTODO</span>
-                        </div>
-                        <button className="btn is-delete">削除</button>
-                    </li>
-                    <li>
-                        <label className="checkbox-label">
-                            <input type="checkbox" className="checkbox-input" />
-                        </label>
-                        <form>
-                            <input
-                                type="text"
-                                className="input"
-                                value="編集中のTODO"
-                            />
-                        </form>
-                        <button className="btn">更新</button>
-                    </li>
-                    <li className="done">
-                        <label className="checkbox-label">
-                            <input type="checkbox" className="checkbox-input" />
-                        </label>
-                        <div>
-                            <span>実行したTODO</span>
-                        </div>
-                        <button className="btn is-delete">削除</button>
-                    </li>
-                    <li>
-                        <label className="checkbox-label">
-                            <input type="checkbox" className="checkbox-input" />
-                        </label>
-                        <div>
-                            <span>ゴミ捨て</span>
-                        </div>
-                        <button className="btn is-delete">削除</button>
-                    </li>
-                    <li>
-                        <label className="checkbox-label">
-                            <input type="checkbox" className="checkbox-input" />
-                        </label>
-                        <div>
-                            <span>掃除</span>
-                        </div>
-                        <button className="btn is-delete">削除</button>
-                    </li>
+                    {tasks.map((task) => (
+                        <li key={task.id}>
+                            <label className="checkbox-label">
+                                <input
+                                    type="checkbox"
+                                    className="checkbox-input"
+                                />
+                            </label>
+                            <div>
+                                <span>{task.title}</span>
+                            </div>
+                            <button className="btn is-delete">削除</button>
+                        </li>
+                    ))}
                 </ul>
             </div>
         </>
