abde7385cecfc56b25ee75df532597a1facf01cc
teraiken <terai.ken@relation2012.com>
2024-02-27 21:34:09 +0900

    add withoutVite to TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,14 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /**
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->withoutVite();
+    }
 }
