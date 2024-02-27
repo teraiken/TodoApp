c1e87558469b8aebcee4d1c9cd160810120780ea
teraiken <terai.ken@relation2012.com>
2024-02-27 21:07:45 +0900

    LaravelにReact.jsとTypeScriptをインストール

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <title>Laravel</title>
+        @viteReactRefresh
+        @vite([
+            'resources/sass/app.scss',
+            'resources/ts/index.tsx',
+            ])
+    </head>
+    <body>
+        <div id="app"></div>
+    </body>
+</html>
