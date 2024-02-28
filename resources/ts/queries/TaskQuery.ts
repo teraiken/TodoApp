3bc701075788ec3b2c46316c313b7ccf78959425
teraiken <terai.ken@relation2012.com>
2024-02-28 09:35:49 +0900

    コンポーネントとロジックの分割

---
+++ resources/ts/queries/TaskQuery.ts
@@ -0,0 +1,8 @@
+import * as api from "../api/TaskAPI";
+import { useQuery } from "react-query";
+
+const useTasks = () => {
+    return useQuery("tasks", () => api.getTasks());
+};
+
+export { useTasks };
